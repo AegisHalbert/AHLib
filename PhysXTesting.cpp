6f55f52685787b7536844d86f3701b9ac3fdec66
Ernestas Jurkus <aegishalbert@gmail.com>
2023-02-23 22:57:19 +0100

    Resources setup PhysX

--- PhysXTesting.cpp
+++ PhysXTesting.cpp
@@ -7,14 +7,28 @@ static physx::PxDefaultAllocator gDefaultAllocatorCallback;
 int main()
 {
 	physx::PxFoundation* mFoundation = PxCreateFoundation(PX_PHYSICS_VERSION, gDefaultAllocatorCallback, gDefaultErrorCallback);
-	if (!mFoundation)
+
+	bool recordMemoryAllocations = true;
+
+	physx::PxPvd* mPvd = physx::PxCreatePvd(*mFoundation);
+	physx::PxPvdTransport* transport = physx::PxDefaultPvdSocketTransportCreate("localhost", 5425, 10);
+	mPvd->connect(*transport, physx::PxPvdInstrumentationFlag::eALL);
+
+	physx::PxPhysics* physics = PxCreatePhysics(PX_PHYSICS_VERSION, *mFoundation, physx::PxTolerancesScale(), recordMemoryAllocations, mPvd);
+
+	if (physics)
 	{
-		std::cout << "Error while initializing" << std::endl;
+		std::cout << "Physics booted up." << std::endl;
 	}
 	else
 	{
-		std::cout << "Correct initialization" << std::endl;
+		std::cout << "Physics failed to start." << std::endl;
 	}
+
+	physics->release();
+	mFoundation->release();
+
+	std::cin.get();
 
 	return 0;
 }
